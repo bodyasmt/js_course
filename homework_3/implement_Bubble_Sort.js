bbf08e214679933693e2d53ee1f4b6fb8f9be4c3
Bohdan Kovalenko <98667222+bodyasmt@users.noreply.github.com>
2022-06-03 16:43:46 +0200

    Update implement_Bubble_Sort.js

--- homework_3/implement_Bubble_Sort.js
+++ homework_3/implement_Bubble_Sort.js
@@ -1,9 +1,11 @@
 function bubbleSort(arr) {
-    let array;
-    for (let i; i % 2; i++ ) {
-      i += array;
+  for(let n = 0; n < arr.length; n++) {
+    for (let i = 0; i < arr.length - 1 - n; i++) {
+      if (arr[i] > arr[i + 1]) {
+        const bubble = arr[i]
+        arr[i] = arr[i + 1]
+        arr[i + 1] = bubble
+      }
     }
-  arr.sort(function(a, b) {
-    return a - b;
-    });
   }
+}
